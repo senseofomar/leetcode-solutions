06720893445fcf61c1abb548ce4c15775ef51c2c
senseofomar <senseofomar@gmail.com>
2025-08-10 00:17:14 +0530

    014

---
+++ Arrays/014-Longest-Common-Prefix.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
