7a815850328dff3fbc56adc4188666c97f6987c0
Omar <senseofomar@users.noreply.github.com>
2025-10-15 21:07:59 +0530

    217

---
+++ Arrays/217-Contains-Duplicate.py
@@ -0,0 +1,25 @@
+def contains_duplicate(nums)-> bool:
+    dupli = []
+    for num in nums:
+        if num not in dupli:
+            dupli.append(num)
+        else:
+            return True
+    return False
+
+def contains_duplicate1(nums) -> bool:
+    seen = set()
+    for num in nums:
+        if num in seen:   # O(1) lookup
+            return True   # duplicate found
+        seen.add(num)     # store it for future checks
+    return False
+
+#one line trick
+def contains_duplicate2(nums) -> bool:
+    return len(nums) != len(set(nums))
+
+numbers = [1,2,3,3,4]
+print(contains_duplicate1(numbers))
+
+
