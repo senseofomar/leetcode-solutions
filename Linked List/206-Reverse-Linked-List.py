1d4d1312a3f9298a53caf136de58cccfa5c3d39a
Omar <senseofomar@users.noreply.github.com>
2025-10-23 23:18:56 +0530

    206 LL

--- Linked List/206-Reverse-Linked-List.py
+++ Linked List/206-Reverse-Linked-List.py
@@ -1,1 +1,12 @@
-#
+#Iterative
+def reverseList(self, head):
+    prev = None  # initially nothing before head
+    curr = head  # start with first node
+
+    while curr:  # while not reached end
+        nxt = curr.next  # temporarily save next node
+        curr.next = prev  # reverse the link
+        prev = curr  # move prev forward
+        curr = nxt  # move curr forward
+
+    return prev  # prev becomes new head
