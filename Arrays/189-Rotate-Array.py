c43481d79e5b844cdd566cad8d846a6660bd8d3f
Omar <senseofomar@users.noreply.github.com>
2025-10-31 23:54:18 +0530

    189 arr

--- Arrays/189-Rotate-Array.py
+++ Arrays/189-Rotate-Array.py
@@ -20,3 +20,21 @@ def rotate1(nums, k):
     reverse(0, k - 1)
     # Step 3: reverse the rest
     reverse(k, n - 1)
+
+# Using Extra Array (Simple but not in-place)
+def rotate2(nums, k):
+    n = len(nums)
+    k %= n  # handle k > n
+
+    def reverse(start, end):
+        while start < end:
+            nums[start], nums[end] = nums[end], nums[start]
+            start += 1
+            end -= 1
+
+    # Step 1: reverse entire array
+    reverse(0, n - 1)
+    # Step 2: reverse first k elements
+    reverse(0, k - 1)
+    # Step 3: reverse the rest
+    reverse(k, n - 1)
