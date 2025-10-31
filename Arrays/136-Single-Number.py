def6fbf93c03bdabfb86fe8de70ee6b486ba3f1a
Omar <senseofomar@users.noreply.github.com>
2025-11-01 00:18:20 +0530

    136 arr

--- Arrays/136-Single-Number.py
+++ Arrays/136-Single-Number.py
@@ -1,5 +1,4 @@
 # Hash Map / Counter
-
 from collections import Counter
 
 def single_number(nums: list[int]) -> int:
@@ -11,3 +10,10 @@ def single_number(nums: list[int]) -> int:
 # Math Trick
 def single_number1(nums: list[int]) -> int:
     return 2 * sum(set(nums)) - sum(nums)
+
+# Bitwise XOR (Best & Optimal)
+def single_number2(nums: list[int]) -> int:
+    result = 0
+    for num in nums:
+        result ^= num  # XOR cancels out duplicates
+    return result
