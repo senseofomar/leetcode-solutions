f5c306fea38e863a387dcc38e40e7873243bbb05
Omar <senseofomar@users.noreply.github.com>
2025-11-06 22:37:39 +0530

    104 tree

--- Trees/104-Maximum-Depth-of-Binary-Tree.py
+++ Trees/104-Maximum-Depth-of-Binary-Tree.py
@@ -5,3 +5,25 @@ def max_depth(root) -> int:
     left = max_depth(root.left)
     right = max_depth(root.right)
     return 1 + max(left, right)
+
+
+
+from collections import deque
+
+
+def max_depth1(root) -> int:
+    if not root:
+        return 0
+
+    queue = deque([root])
+    depth = 0
+
+    while queue:
+        depth += 1
+        for _ in range(len(queue)):  # process one level
+            node = queue.popleft()
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+    return depth
