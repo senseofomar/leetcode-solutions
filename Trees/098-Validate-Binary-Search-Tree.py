54b0fd9fe9323852240c92dd8f387f8ce9a318c6
Omar <senseofomar@users.noreply.github.com>
2025-11-07 21:47:18 +0530

    98 tree

---
+++ Trees/098-Validate-Binary-Search-Tree.py
@@ -0,0 +1,21 @@
+def is_valid_bst(root) -> bool:
+    def helper(node, low=float('-inf'), high=float('inf')):
+        if not node:
+            return True
+
+        val = node.val
+        # check current node
+        if val <= low or val >= high:
+            return False
+
+        # check left subtree (values < node.val)
+        if not helper(node.left, low, val):
+            return False
+
+        # check right subtree (values > node.val)
+        if not helper(node.right, val, high):
+            return False
+
+        return True
+
+    return helper(root)
