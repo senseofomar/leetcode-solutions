6f5a832e5be2036cf52e670225efa1cfa6fc7e35
Omar <senseofomar@users.noreply.github.com>
2025-11-17 00:03:34 +0530

    121 arr

---
+++ Dynamic Programming/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,11 @@
+def maxProfit(prices):
+    min_price = float('inf')
+    max_profit = 0
+
+    for p in prices:
+        if p < min_price:
+            min_price = p
+        else:
+            max_profit = max(max_profit, p - min_price)
+
+    return max_profit
