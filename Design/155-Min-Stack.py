aeb18b4cf88fc7a0d5bba6e072c14638c8ee973f
Omar <senseofomar@users.noreply.github.com>
2025-11-20 20:21:53 +0530

    155

---
+++ Design/155-Min-Stack.py
@@ -0,0 +1,22 @@
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # If min_stack empty OR this value is <= current minimum
+        if not self.min_stack or val <= self.min_stack[-1]:
+            self.min_stack.append(val)
+
+    def pop(self) -> None:
+        val = self.stack.pop()
+        if val == self.min_stack[-1]:
+            self.min_stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def get_min(self) -> int:
+        return self.min_stack[-1]
