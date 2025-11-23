48e69ce82df1baec760ed97a1453017f23b1c526
Omar <senseofomar@users.noreply.github.com>
2025-11-23 23:03:18 +0530

    013

--- Math/013-Roman-To-Integer.py
+++ Math/013-Roman-To-Integer.py
@@ -32,3 +32,27 @@ def roman_to_int2(s: str) -> int:
             total += values[s[i]]
 
     return total
+
+def roman_to_int3(s: str) -> int:
+    specials = {
+        "IV":4, "IX":9,
+        "XL":40, "XC":90,
+        "CD":100, "CM":900
+    }
+    values = {
+        'I':1, 'V':5, 'X':10, 'L':50,
+        'C':100, 'D':500, 'M':1000
+    }
+
+    i = 0
+    total = 0
+
+    while i < len(s):
+        if i + 1 < len(s) and s[i:i+2] in specials:
+            total += specials[s[i:i+2]]
+            i += 2
+        else:
+            total += values[s[i]]
+            i += 1
+
+    return total
