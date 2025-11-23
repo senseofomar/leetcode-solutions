fc6a783353002a506687796b9085ad6f43b7f108
Omar <senseofomar@users.noreply.github.com>
2025-11-23 23:02:31 +0530

    013

--- Math/013-Roman-To-Integer.py
+++ Math/013-Roman-To-Integer.py
@@ -17,3 +17,17 @@ def roman_to_int(s: str) -> int:
         prev = curr
 
     return total
+
+def roman_to_int2(s: str) -> int:
+    values = {'I':1, 'V':5, 'X':10, 'L':50,
+              'C':100, 'D':500, 'M':1000}
+
+    total = 0
+
+    for i in range(len(s)):
+        if i + 1 < len(s) and values[s[i]] < values[s[i+1]]:
+            total -= values[s[i]]       # subtraction rule
+        else:
+            total += values[s[i]]
+
+    return total
