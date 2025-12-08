af621eff0a6b7b09ed794b038c593a388815e6b5
Omar <senseofomar@users.noreply.github.com>
2025-12-08 10:58:07 +0530

    revision 1

--- Arrays/001-Two-Sum.py
+++ Arrays/001-Two-Sum.py
@@ -2,17 +2,31 @@
 # Difficulty: Easy
 # Topic: Arrays, Hash Table
 
-class Solution(object):
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        seen = {}
-        for i in range(len(nums)):
-            num = nums[i]
-            complement = target - num
-            if complement in seen:
-                return [seen[complement], i]
-            seen[num] = i
+# class Solution(object):
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         """
+#         :type nums: List[int]
+#         :type target: int
+#         :rtype: List[int]
+#         """
+#         seen = {}
+#         for i in range(len(nums)):
+#             num = nums[i]
+#             complement = target - num
+#             if complement in seen:
+#                 return [seen[complement], i]
+#             seen[num] = i
+
+
+def two_sum_brute(nums : list[int], target : int) -> tuple[int, int] | None:
+    for i in range(len(nums)):
+        num = nums[i]
+        complement = target - num
+        for j in range(len(nums)):
+            num = nums[j]
+            if j != i and nums[j] == complement:
+                return i, j
+    return None
+
+
+print (two_sum_brute([1,2,4], 3))
