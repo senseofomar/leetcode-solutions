6efe756cca48dec6b65b8b71c1bc3fe637d1562a
Omar <senseofomar@users.noreply.github.com>
2026-01-01 23:16:14 +0530

    268

--- Others/268-Missing-Number.py
+++ Others/268-Missing-Number.py
@@ -1,5 +1,19 @@
+# Sum Formula Math
 def missing_number(nums: list[int]) -> int:
     n = len(nums)
     expected = n * (n + 1) // 2
     actual = sum(nums)
     return expected - actual
+
+# Best Xor
+def missing_number1(nums: list[int]) -> int:
+    result = 0
+    n = len(nums)
+
+    for i in range(n + 1):
+        result ^= i
+
+    for num in nums:
+        result ^= num
+
+    return result
