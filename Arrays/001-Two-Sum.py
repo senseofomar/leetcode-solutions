86faab2198a411072c296aa7df12e197dca1e0d4
Omar <senseofomar@users.noreply.github.com>
2026-01-21 23:43:48 +0530

    cleaning the two sum approach

--- Arrays/001-Two-Sum.py
+++ Arrays/001-Two-Sum.py
@@ -1,21 +1,4 @@
-# Problem: https://leetcode.com/problems/two-sum/
-# Difficulty: Easy
-# Topic: Arrays, Hash Table
 
-# class Solution(object):
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#         """
-#         :type nums: List[int]
-#         :type target: int
-#         :rtype: List[int]
-#         """
-#         seen = {}
-#         for i in range(len(nums)):
-#             num = nums[i]
-#             complement = target - num
-#             if complement in seen:
-#                 return [seen[complement], i]
-#             seen[num] = i
 
 
 def two_sum_brute(nums : list[int], target : int) -> tuple[int, int] | None:
