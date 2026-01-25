dded74a54674b7fcb60dfb79729c7d05c37670b4
Omar <senseofomar@users.noreply.github.com>
2026-01-25 19:21:02 +0530

    valid sudoku

--- Arrays/036-Valid-Sudoku.py
+++ Arrays/036-Valid-Sudoku.py
@@ -27,16 +27,3 @@ def is_valid_sudoku(board: list[list[str]]) -> bool:
 
         return True
 
-def is_valid_sudoku1(board):
-    seen = set()
-    for r in range(9):
-        for c in range(9):
-            val = board[r][c]
-            if val == '.': continue
-            row_key = f"row{r}-{val}"
-            col_key = f"col{c}-{val}"
-            box_key = f"box{r//3}-{c//3}-{val}"
-            if row_key in seen or col_key in seen or box_key in seen:
-                return False
-            seen.update((row_key, col_key, box_key))
-    return True
