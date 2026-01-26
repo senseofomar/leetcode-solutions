f4e94c941221c7f32fd014ff3caa62130a597af2
Omar <senseofomar@users.noreply.github.com>
2026-01-26 12:33:44 +0530

    single number

--- Arrays/136-Single-Number.py
+++ Arrays/136-Single-Number.py
@@ -1,11 +1,4 @@
-# Hash Map / Counter
-from collections import Counter
 
-def single_number(nums: list[int]) -> int:
-    count = Counter(nums)
-    for num, freq in count.items():
-        if freq == 1:
-            return num
 
 # Math Trick
 def single_number1(nums: list[int]) -> int:
